d870621c54b7e68946c8f2d16f99208cfb5f9e47
Aniket Gupta <aniket.gupta46@gmail.com>
2017-11-08 03:21:28 +0530

    Added new problem solutions

---
+++ src/main/java/com/aniket/ctci/arrayandstring/LongestPalindromicSubstring.java
@@ -0,0 +1,63 @@
+package com.aniket.ctci.arrayandstring;
+
+public class LongestPalindromicSubstring {
+    /**
+     * Longest Palindromic Substring
+     * Given a string S, find the longest palindromic substring in S.
+
+     Substring of string S:
+
+     S[i...j] where 0 <= i <= j < len(S)
+
+     Palindrome string:
+
+     A string which reads the same backwards. More formally, S is palindrome if reverse(S) = S.
+
+     Incase of conflict, return the substring which occurs first ( with the least starting index ).
+
+     Example :
+
+     Input : "aaaabaaa"
+     Output : "aaabaaa"
+
+     */
+
+    public String longestPalindrome(String a) {
+
+        int maxLen = 1;
+        int startIndex = 0;
+
+        for(int i = 1; i < a.length(); i++) {
+
+            // for even len
+            int low = i - 1;
+            int high = i;
+
+            while(low >= 0 && high < a.length() && a.charAt(low) == a.charAt(high)) {
+                if(high - low + 1 > maxLen) {
+                    maxLen = high - low + 1;
+                    startIndex = low;
+                }
+                low--;
+                high++;
+            }
+
+
+            // for odd len
+            low = i - 1;
+            high = i + 1;
+
+            while(low >= 0 && high < a.length() && a.charAt(low) == a.charAt(high)) {
+                if(high - low + 1 > maxLen) {
+                    maxLen = high - low + 1;
+                    startIndex = low;
+                }
+                low--;
+                high++;
+            }
+        }
+
+        return a.substring(startIndex, startIndex + maxLen);
+
+    }
+}
